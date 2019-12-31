3ac5a3a6bf79810b40618858718793ce468a93a9
nahelouQ <naheloumaveille@gmail.com>
2019-12-31 17:06:52 +0100

    Updates

---
+++ precache-manifest.ffbddecd239cea9580734381e5c6bfd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb27d03753de55facf839f4efc37c36a",
+    "url": "/apps/index.html"
+  },
+  {
+    "revision": "126a4c8f8075ee8d518b",
+    "url": "/apps/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cb00de41366120efa94f",
+    "url": "/apps/static/js/2.78313752.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/apps/static/js/2.78313752.chunk.js.LICENSE"
+  },
+  {
+    "revision": "126a4c8f8075ee8d518b",
+    "url": "/apps/static/js/main.a5505353.chunk.js"
+  },
+  {
+    "revision": "ac71ab88a216aa42168f",
+    "url": "/apps/static/js/runtime-main.0bd71be2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/apps/static/media/logo.5d5d9eef.svg"
+  }
+]);
