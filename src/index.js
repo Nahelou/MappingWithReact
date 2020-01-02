18253f496f25d27810abb974c07dfb52ccccbd93
Nahelou <naheloumaveille@gmail.com>
2020-01-02 14:32:22 +0100

    Map zoomToNantes
    Co-Authored-By: null <Nahelou@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -10,19 +10,17 @@ import {StaticMap} from 'react-map-gl';
 // ReactDOM.render(<App />, document.getElementById('root'));
 /// app.js
 
-// Set your mapbox access token here
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibmFoZWxvdSIsImEiOiJjamVvZHc5N24zazU2MzNwa2N4cWppa2lsIn0.5_rLbZlODCllJudZrubOcA';
 
-// Initial viewport settings
 const initialViewState = {
-  longitude: -122.41669,
-  latitude: 37.7853,
+  longitude: -1.549780,
+  latitude: 47.216241,
   zoom: 13,
   pitch: 0,
   bearing: 0
 };
 
-// Data to be used by the LineLayer
+
 const data = [{sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}];
 
 class App extends React.Component {
@@ -44,7 +42,5 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
