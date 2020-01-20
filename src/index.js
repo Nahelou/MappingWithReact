2b926c4ee00f9fbb3b5b18773cb5d98f76ad0a84
Nahelou <naheloumaveille@gmail.com>
2020-01-20 16:27:04 +0100

    add Tweets

--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
-import Test from './test';
 
-
-// import App from './App';
-// ReactDOM.render(<App />, document.getElementById('root'));
-/// app.js
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Test />, document.getElementById('Loading'));
 
-serviceWorker.unregister();
+// serviceWorker.unregister();
